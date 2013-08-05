29a41e3de0634827962f2d7e3f1675ce37cddb2a
Ian <ian@zhaoyan.me>
2013-08-05 23:49:24 +0800

    add on fancybox pugin

--- HTML5Application/public_html/js/mapScript.js
+++ HTML5Application/public_html/js/mapScript.js
@@ -3,17 +3,52 @@
  * and open the template in the editor.
  */
 $('document').ready(function(){
+    $('div .details').hide();
     $('a.dot').click(function(){
        $('a.dot').removeClass('selected');
        $(this).addClass('selected');
-       $('#bgPanel').addClass('bg_popup');
+       $('#bgPanel').addClass('bg_popup');          
+         var location = $(this).attr('location');
+       $.ajax({
+                url: "../HTML5Application/details/"+location+".html",
+                context: document.body
+            }).done(function(img){
+                $('#descrption').html(img);
+            });
+             $('div.details').show();
+              $('div.details').addClass('detailsContent');
+         
     });    
-    
-     $('#bgPanel').click(function(){
+    $('a.dot').mouseover(function(){
+
+    });
+    $('a.dot').mouseout(function(){
+        $(this).html("");
+    });
+    $('#bgPanel').click(function(){
          $('a.dot').removeClass('selected');
-         $(this).removeClass('bg_popup');     
+         $(this).removeClass('bg_popup'); 
+          $('div.details').hide();
          
      });
-    
+     
+     	$('.fancybox-thumbs').fancybox({
+				prevEffect : 'none',
+				nextEffect : 'none',
+				closeBtn  : true,
+				arrows    : false,
+				nextClick : true,
+                                opacity : 0.9,
+				helpers : {
+					thumbs : {
+						width  : 50,
+						height : 50          
+					},
+                                        overlay : {
+                                             opacity    : 0.9
+                                            }
+				}
+			});
+         
 });
 
