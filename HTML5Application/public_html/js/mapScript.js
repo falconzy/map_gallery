adbd75d39a9835f70529d01ac0571328e617d522
Zhao Yan <ian@zhaoyan.me>
2013-08-06 19:08:56 +0800

    Get picture from flickr API

--- HTML5Application/public_html/js/mapScript.js
+++ HTML5Application/public_html/js/mapScript.js
@@ -9,12 +9,21 @@ $('document').ready(function(){
        $(this).addClass('selected');
        $('#bgPanel').addClass('bg_popup');          
          var location = $(this).attr('location');
-       $.ajax({
-                url: "details/"+location+".html",
-                context: document.body
-            }).done(function(img){
-                $('#descrption').html(img);
-            });
+         var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=091acd7b8c4d6d38686d24bb4d3a522c&privacy_filter=1&text="+location+"&sort=interestingness-desc&per_page=12&page=1&format=json&nojsoncallback=1";
+         $('#descrption').html('');
+          $.getJSON(url, function(data) {
+			      var items = [];
+            var photoDetails = data;
+			       $.each(data.photos.photo, function(key, val) {
+			       
+                    var photoURL_b = "http://farm"+val.farm+".staticflickr.com/"+val.server+"/"+val.id+"_"+val.secret+"_b.jpg";
+                    var photoURL_S = "http://farm"+val.farm+".staticflickr.com/"+val.server+"/"+val.id+"_"+val.secret+"_q.jpg";
+				            items.push('<a class="fancybox-thumbs" data-fancybox-group="thumb" href='+photoURL_b+' ><img src='+photoURL_b+' width="150" height="150" alt="DSC_0009"></a>');
+			        });
+				        $('<div/>', {
+					      html: items.join('')
+					      }).appendTo('#descrption');
+	          });
              $('div.details').show();
               $('div.details').addClass('detailsContent');
          
