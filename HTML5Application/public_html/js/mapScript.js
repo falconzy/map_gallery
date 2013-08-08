dbe9c66b873d52fafa4fe7d1d70263317f15e03c
Ian <ian@zhaoyan.me>
2013-08-08 18:19:48 +0800

    - Change HTML headder  - modified index layout  - add fade in and fade out event on picture showing  - add mobile support

--- HTML5Application/public_html/js/mapScript.js
+++ HTML5Application/public_html/js/mapScript.js
@@ -4,35 +4,48 @@
  */
 $('document').ready(function(){
     $('div .details').hide();
-    $('a.dot').click(function(){
+    $('a.dot').click(function(){ 
        $('a.dot').removeClass('selected');
-       $(this).addClass('selected');
-       $('#bgPanel').addClass('bg_popup');          
-         var location = $(this).attr('location');
+       $(this).addClass('selected');      
+       var location = $(this).attr('location');
        $.ajax({
                 url: "details/"+location+".html",
                 context: document.body
             }).done(function(img){
                 $('#descrption').html(img);
             });
-             $('div.details').show();
-              $('div.details').addClass('detailsContent');
-         
-    });    
+             $('#bgPanel').addClass('bg_popup'); 
+             $('#bgPanel').hide();
+             $('#bgPanel').fadeIn(1000,function(){               
+                $('div.details').addClass('detailsContent');   
+             });
+             $('div.details').fadeIn(900);
+             $('#bgPanel').data('display','true');    
+             
+     }); 
     $('a.dot').mouseover(function(){
 
     });
     $('a.dot').mouseout(function(){
         $(this).html("");
     });
-    $('#bgPanel').click(function(){
-         $('a.dot').removeClass('selected');
-         $(this).removeClass('bg_popup'); 
-          $('div.details').hide();
-         
+    $('#backgroud').click(function(){        
+        if($('#bgPanel').data('display')==='false')
+            {            
+                $('a.dot').removeClass('selected');
+                $('#bgPanel').fadeOut(1000,function(){
+                      $('#bgPanel').removeClass('bg_popup');  
+                 });
+                $('#bgPanel').data('display','true');  
+                $('div.details').fadeOut(900);
+                            
+      }
+      else
+          {
+               $('#bgPanel').data('display','false');
+          }
      });
-     
-     	$('.fancybox-thumbs').fancybox({
+    $('.fancybox-thumbs').fancybox({
 				prevEffect : 'none',
 				nextEffect : 'none',
 				closeBtn  : true,
@@ -49,6 +62,7 @@ $('document').ready(function(){
                                             }
 				}
 			});
+    $( "#map" ).draggable();
          
 });
 
